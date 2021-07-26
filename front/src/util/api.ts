aa8890a9467242a71f439ba19cfba6f905068448
jigglypop <52653682+jigglypop@users.noreply.github.com>
2021-07-26 16:48:56 +0900

    0726

---
+++ front/src/util/api.ts
@@ -0,0 +1,19 @@
+
+export const API_ENDPOINT = process.env.API_ENDPOINT;
+
+
+export default function fetchThen(URL: string, data: object) {
+// 
+//   return fetch(`${API_ENDPOINT}${URL}`, data)
+//     .then(res => {
+//       const token = res.headers.get('token')
+//       if (token) {
+//         cache.set("token", {
+//           value: token
+//         })
+//       }
+//       return res.json()
+//     }
+//     )
+}
+
