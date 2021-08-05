daf9721f30ebcc21f6130a7a667d09cf08656c12
hwookim <gusdn9754@gmail.com>
2021-08-05 14:40:22 +0900

    feat: auth 내 상황에 맞는 status code 발급

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,6 +1,5 @@
 import bcrypt from "bcryptjs";
 import { Request, Response } from "express";
-import { Model } from "sequelize-typescript";
 import User from "../models/User";
 import HttpError from "../errors/HttpError";
 import { generateToken } from "../util/generateToken";
@@ -16,22 +15,28 @@ interface IUpdateUserImgRequest extends IAuthRequest {
 export const check = async (req: IAuthRequest, res: Response) => {
   const { userId } = req.body;
   const user = await User.findByPk(userId);
-  if (!user) throw new HttpError(400, "같은 이름의 계정이 존재하지 않습니다.");
+  if (!user) {
+    throw new HttpError(401, "같은 이름의 계정이 존재하지 않습니다.");
+  }
   res.status(200).json({ data: user });
 };
 
 export const login = async (req: Request, res: Response) => {
   const { username, password } = req.body;
-  if (!username || !password)
+  if (!username || !password) {
     throw new HttpError(400, "이름과 비밀번호를 정확히 입력해 주세요");
+  }
   const user = await User.findOne({ where: { username } });
-  // 계정이 존재하는지
-  if (!user) throw new HttpError(400, "같은 이름의 계정이 존재하지 않습니다.");
-  // 비밀번호가 일치하지 않음
+  if (!user) {
+    throw new HttpError(401, "같은 이름의 계정이 존재하지 않습니다.");
+  }
+
   const hashedPassword = user.getDataValue("hashedPassword");
   const valid = await bcrypt.compare(password, hashedPassword);
-  if (!valid) throw new HttpError(400, "비밀번호가 잘못되었습니다");
-  // 토큰 발급
+  if (!valid) {
+    throw new HttpError(401, "비밀번호가 잘못되었습니다");
+  }
+
   const serialized = await serialize(user);
   const token = await generateToken(user);
   res.set("token", token);
