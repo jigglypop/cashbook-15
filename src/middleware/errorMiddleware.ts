fa523b54533c5531d47ed75b7b67a119d041fbd0
hwookim <gusdn9754@gmail.com>
2021-08-05 15:11:11 +0900

    feat: DB 에러 핸들링

--- src/middleware/errorMiddleware.ts
+++ src/middleware/errorMiddleware.ts
@@ -1,5 +1,22 @@
 import { NextFunction, Request, Response } from "express";
 import HttpError from "../errors/HttpError";
+import { BaseError, UniqueConstraintError } from "sequelize";
+
+const handleDBError = (error: BaseError, res: Response) => {
+  let status = 500;
+  let message = "DB Error: " + error.message;
+
+  if (error instanceof UniqueConstraintError) {
+    status = 400;
+    message =
+      "DB Unique Error: " +
+      Object.keys(error.fields)
+        .map((field) => field.split(".")[1])
+        .join(", ") +
+      "이(가) 중복됩니다.";
+  }
+  res.status(status).json({ status, message });
+};
 
 const errorMiddleware = (
   error: Error,
@@ -11,6 +28,10 @@ const errorMiddleware = (
   let status = 500;
   let message = "Server Error: " + error.message;
 
+  if (error instanceof BaseError) {
+    handleDBError(error, res);
+    return;
+  }
   if (error instanceof HttpError) {
     status = error.status;
     message = error.message;
